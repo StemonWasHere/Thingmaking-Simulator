b7eee15cc191557d11f4cdaac65ea8adc9015178
StemonWasHere <109590736+StemonWasHere@users.noreply.github.com>
2025-02-11 22:24:45 +0700

    adding clear function and dashboard

--- main.py
+++ main.py
@@ -1,14 +1,35 @@
+from os import system, name
+
 thing = 0
 
+#clear func
+def clr():
+# windows
+    if name == 'nt':
+        _ = system('cls')
+# macos and linux
+    else:
+         _ = system('clear')
+
+#printing main dashboard
+def dash():
+    global thing
+    print("-x-x-x-x-x-x{Thingmaking Factory}x-x-x-x-x-x-")
+    print("Things make:", thing)
+    print("-x-x-x-x-x-x{Thingmaking Factory}x-x-x-x-x-x-")
+
+#make func
 def make():
     global thing
     maketxt = input(":")
     if maketxt.lower() == "make":
        thing = thing + 1
-       print(thing)
+       clr()
+       dash()
        make()
     else:
-       print(thing)
+       clr()
+       dash()
        make()
 
 make()
