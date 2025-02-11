9cd10adba7c854e9885e8502a51ca299f764cfb9
StemonWasHere <109590736+StemonWasHere@users.noreply.github.com>
2025-02-11 21:43:12 +0700

    simple make function

--- main.py
+++ main.py
@@ -1,3 +1,14 @@
-#wip
+thing = 0
 
-print("wip")
+def make():
+    global thing
+    maketxt = input(":")
+    if maketxt.lower() == "make":
+       thing = thing + 1
+       print(thing)
+       make()
+    else:
+       print(thing)
+       make()
+
+make()
